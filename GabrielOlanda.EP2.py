0bb942917e184b217b993a00293783e6109108aa
gabsolanda <gabs.olanda@gmail.com>
2015-03-24 19:24:39 -0300

    GabrielOlanda3_EP2
    Commit3

--- GabrielOlanda.EP2.py
+++ GabrielOlanda.EP2.py
@@ -26,55 +26,88 @@ for e in lista:
 escolhaPC = choice(Llimpa)
 c = len(escolhaPC)
 print(escolhaPC)
-turtle = turtle.Turtle()  # Cria um objeto "desenhador"
+grade = turtle.Turtle()  # Cria um objeto "desenhador"
 
 ######################################################
 
 #Desenhando a trave da forca
 
 for i in range(1):
-    turtle.penup()
-    turtle.setpos(-300,0)
-    turtle.pendown()
-    turtle.speed(5)
-    turtle.pensize(5)
+    grade.penup()
+    grade.setpos(-300,0)
+    grade.pendown()
+    grade.speed(5)
+    grade.pensize(5)
     dist = 150
     ang = 90
-    turtle.left(ang)
-    turtle.forward(dist)
-    turtle.right(ang)
-    turtle.forward(80)
-    turtle.right(ang)
-    turtle.forward(35)
+    grade.left(ang)
+    grade.forward(dist)
+    grade.right(ang)
+    grade.forward(80)
+    grade.right(ang)
+    grade.forward(35)
     
-    turtle.penup()
-    turtle.setpos(-185,0)
-    turtle.left(90)
+    grade.penup()
+    grade.setpos(-185,0)
+    grade.left(90)
     
-
     
 ######################################################
     
 # Fazendo os espaços das letras
 
 for i in range(c):
-    turtle.speed(1)
-    turtle.color("blue")
-    turtle.pendown()
-    turtle.forward(20)
-    turtle.penup()
-    turtle.forward(17)
-    turtle.pendown()
+    grade.speed(300)
+    grade.color("blue")
+    grade.pendown()
+    grade.forward(20)
+    grade.penup()
+    grade.forward(17)
+    grade.pendown()
+    
+    
     
 ####################################################
+    # Fazendo o corpo - Cabeça
+cabeca = turtle.Turtle()
+cabeca.color("white")
+cabeca.pensize(2)
+cabeca.penup()
+cabeca.setpos(-220,93)
+cabeca.pendown()
+cabeca.circle(10)
+
+####################################################
+    # Fazendo o corpo = Tórax
+tor = turtle.Turtle()
+tor.color("white")
+tor.pensize(2)
+tor.penup()
+tor.setpos(-220,93)
+tor.pd()
+tor.right(90)
+tor.forward(50)
+
+####################################################
+    # Fazendo o corpo = Perda da Direita
+pernad = turtle.Turtle()
+pernad.color("white")
+pernad.pensize(2)
+pernad.penup()
+pernad.setpos(-220,43)
+pernad.pd()
+pernad.right(60)
+pernad.forward(25)
 
     
-letra = window.textinput("Enforcando-se", "Escolha uma letra que acha que está na palavra.")
+#letra = window.textinput("Enforcando-se", "Escolha uma letra que acha que está na palavra.")
 
-while len(letra) != 1:
-    letra = window.textinput("Enforcando-se", "Escolha uma letra que acha que está na palavra.")
+#while len(letra) != 1:
+    #letra = window.textinput("Enforcando-se", "Escolha uma letra que acha que está na palavra.")
 
 #if letra in escolhaPC:
+    #turtle.write
+    
 
 
 
